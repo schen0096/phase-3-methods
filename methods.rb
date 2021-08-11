c8cfb3ed99462a0b38cd445cb079ddafbdd2cbda
schen0096 <85653202+schen0096@users.noreply.github.com>
2021-08-11 10:40:08 -0400

    Done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
 # Your code here!
+def greet_programmer
+    "Hello, programmer!"
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name=nil)
+    if name
+        puts "Hello, #{name}!"
+    else 
+        puts "Hello, programmer!"
+    end
+end
+
+def add(a,b)
+    return a + b
+end
+
+def halve(a)
+    if a.class != Integer
+        return nil
+    else 
+        return a/2
+    end
+end
